41b3684034ff784f2f0570c35786310c37a0a94f
pyjun01 <pyjun01@naver.com>
2018-11-21 14:26:23 +0900

    modify

--- d2/list3/js/script.js
+++ d2/list3/js/script.js
@@ -1,11 +1,11 @@
 window.onload= _ =>{
-	let Q= document.querySelectorAll(".faq_box h2");
+	let Q= document.querySelectorAll(".faq_box>ul>li>a");
 	for(let tag of Q){
 		tag.addEventListener("click", function (){
 			this.nextElementSibling.classList.toggle("focus");
 		});
 	}
-	let all= document.querySelector(".faq_box>.head>.click");
+	let all= document.querySelector(".faq_box>.head>a");
 	all.addEventListener("click", function (){
 		let focus= document.querySelectorAll(".focus");
 		for(let tag of focus){
