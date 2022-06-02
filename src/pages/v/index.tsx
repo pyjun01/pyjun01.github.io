be6384d8fb061b4d5583613e7e2d0a4b3cf7ef1b
pyjun01 <pyjun02@gmail.com>
2022-06-02 16:39:11 +0900

    Update main info

--- src/pages/v/index.tsx
+++ src/pages/v/index.tsx
@@ -13,10 +13,23 @@ function BlogPage() {
           dir: 'ltr',
           lang: 'ko',
         }}
+        title='pyjun01 개발 블로그'
       >
         <meta name='theme-color' content='#1a5b89' />
         <meta name='description' content='pyjun01 개발 블로그' />
         <meta name='keywords' content='pyjun01,개발,개발자,개발 블로그' />
+        <meta name='naver-site-verification' content='2a28f13354877854bb05feb3be7bf8f34ff88f05' />
+        <meta name='theme-color' content='#1a5b89' />
+        <meta property='og:title' content='pyjun01 개발 블로그' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://pyjun01.github.io' />
+        <meta property='og:description' content='pyjun01의 개발 블로그입니다' />
+        <meta property='og:site_name' content='pyjun01 blog' />
+        <meta property='og:locale' content='ko_KR' />
+        <meta name='description' content='pyjun01의 개발 블로그입니다' />
+        <meta name='twitter:card' content={'summary'} />
+        <meta name='twitter:title' content='pyjun01 개발 블로그' />
+        <meta name='twitter:description' content='pyjun01의 개발 블로그입니다' />
         <meta name='naver-site-verification' content='2a28f13354877854bb05feb3be7bf8f34ff88f05' />
       </Helmet>
       <List nodes={data.allMdx.nodes} />
