47feabe547bb5e8acabaa7557c5029634ab69395
pyjun01 <pyjun02@gmail.com>
2022-06-14 23:27:10 +0900

    Update SEO setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import BlogPage from './v';
+import { Helmet } from 'react-helmet';
+
+import { usePostList } from '../hooks/post';
+import Layout from '../components/layout';
+import List from '../components/List';
 
 function IndexPage() {
-  return <BlogPage />;
+  const data = usePostList();
+
+  return (
+    <Layout>
+      <Helmet
+        htmlAttributes={{
+          dir: 'ltr',
+          lang: 'ko',
+        }}
+        title='pyjun01 개발 블로그'
+      >
+        <meta property='og:title' content='pyjun01 개발 블로그' />
+        <meta property='og:url' content='https://pyjun01.github.io' />
+        <meta property='og:description' content='pyjun01의 개발 블로그입니다' />
+        <meta name='description' content='pyjun01의 개발 블로그입니다' />
+        <meta name='keywords' content='javascript,react,자바스크립트,리액트,pyjun01,개발,개발자,개발 블로그' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:title' content='pyjun01 개발 블로그' />
+        <meta name='twitter:description' content='pyjun01의 개발 블로그입니다' />
+      </Helmet>
+      <List nodes={data.allMdx.nodes} />
+    </Layout>
+  );
 }
 
 export default IndexPage;
