67668b491c0125e6bb94e7ad294ecfcd057bef99
pyjun01 <pyjun02@gmail.com>
2024-12-25 01:39:16 +0900

    Updates

---
+++ component---src-pages-404-tsx-a73c5df5cc5bce9a34fa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpyjun01_blog=self.webpackChunkpyjun01_blog||[]).push([[453],{731:function(n,u,e){e.r(u);var l=e(6540);u.default=function(){return(0,l.useEffect)((()=>{window.location.replace("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-a73c5df5cc5bce9a34fa.js.map
